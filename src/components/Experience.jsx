46432e0af4c0308686d9111ac6b300b25067c690
akrama23 <arundelfootball23@gmail.com>
2024-01-25 02:00:27 -0500

    edits

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,41 +1,48 @@
-import { Environment, OrbitControls, Sky } from "@react-three/drei";
+import {
+  ContactShadows,
+  Environment,
+  OrbitControls,
+  Sky,
+} from "@react-three/drei";
 import { Avatar } from "./Avatar";
 import { useControls } from "leva";
 import { MeshStandardMaterial } from "three";
 
-
 export const Experience = () => {
-
-    const { animation } = useControls({
-      animation: {
-        value: "Typing",
-        options: ["Typing", "Standing", "Falling"]
-      },
-    });
+  const { animation } = useControls({
+    animation: {
+      value: "Typing",
+      options: ["Typing", "Standing", "Falling"],
+    },
+  });
 
   return (
     <>
       <OrbitControls />
-      <Sky/>
-      <Environment preset="sunset"/>
+      <Sky />
+      <Environment preset="sunset" />
       <group position-y={-1}>
-      <Avatar animation={animation}/>
-      {
-        animation === "Typing" && (
+        <ContactShadows
+          opacity={0.45}
+          scale={10}
+          blur={1}
+          far={10}
+          resolution={256}
+          color="#000000"
+        />
+        <Avatar animation={animation} />
+        {animation === "Typing" && (
           <mesh scale={[0.8, 0.5, 0.8]} position-y={0.15}>
-        <boxGeometry/>
-        <meshStandardMaterial color="white"/>
-      </mesh>
-        )
-      }
-      
-      <mesh 
-      scale={5}
-      rotation-x={-Math.PI * 0.5 }>
-        <planeGeometry/>
-        <meshStandardMaterial color="white"/>
-      </mesh>
-        </group>
+            <boxGeometry />
+            <meshStandardMaterial color="white" />
+          </mesh>
+        )}
+
+        <mesh scale={5} rotation-x={-Math.PI * 0.5} position-y={-0.001}>
+          <planeGeometry />
+          <meshStandardMaterial color="white" />
+        </mesh>
+      </group>
     </>
   );
 };
